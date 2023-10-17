dfb26dd0ceeb0b79f1adae7dc6cc3e2c62f074a4
Muhammad Abdullah <muhammadabdullah32196@gmail.com>
2023-10-17 23:24:27 +0500

    connect to database

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import mysql.connector as connector
 
+con = connector.connect(host="localhost",
+                  user = "root",
+                  password = "root",
+                  database = "classicmodels")
+
+if con.is_connected():
+    print("Successfully connected....")
+    
+    
+cursor = con.cursor()
+
+
+    
 
