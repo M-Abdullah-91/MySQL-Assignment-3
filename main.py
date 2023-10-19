1adeb3b7b003c949ae82e2418ae0de106ed155c2
Muhammad Abdullah <muhammadabdullah32196@gmail.com>
2023-10-19 09:46:02 +0500

    new functions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pymysql
 import query
 
+
+    
 conn = pymysql.connect(
         host='localhost',
         user='root',
@@ -11,6 +13,45 @@ conn = pymysql.connect(
 
 cursor = conn.cursor()
 
-query.add_employee(conn,102, "zain", "Ahmed", "x01","zainahmed@gmail.com", 2, 1002,"President")
+print("""
+            press 1 to add employee
+            press 2 to add customers
+            press 3 to add orders
+            press 4 to add payment detail
+            press 5 to show dashboard stats
+            press 6 to show all employees
+            press 7 to show all employees (name) along with the city and country name they are working and their job title
+            press 8 to show all product name and price and product category name
+            press 9 to show all employees name who captures the customer
+            press 10 to show all employees who didn't suceed in capturing the customer
+            press 11 to show the employee names along with the sale they managed to capture by customer""")
+    
+user_input = int(input("Enter a Number: "))
+    
+if user_input == 1:
+    query.add_employee(conn)
+elif user_input == 2:
+    query.add_customer(conn)
+elif user_input == 3:
+    pass
+elif user_input == 4:
+    pass
+elif user_input == 5:
+    query.show_stats(conn)
+elif user_input == 6:
+    pass
+elif user_input == 7:
+    pass
+elif user_input == 8:
+    pass
+elif user_input == 9:
+    pass
+elif user_input == 10:
+    pass
+elif user_input == 11:
+    pass
+else :
+    print("End..")
+# query.add_employee(conn,102, "zain", "Ahmed", "x01","zainahmed@gmail.com", 2, 1002,"President")
 
 
--- query.py
+++ query.py
@@ -1,8 +1,83 @@
-def add_employee(conn, employeeNumber, lastName, firstName, extension, email, officeCode, reportsTo, jobTitle):
+from datetime import date
+
+def add_employee(conn):
+    employeeNumber = input("Enter employeeNumber: ") 
+    lastName = input("Enter last name: ")
+    firstName = input("Enter first name: ")
+    extension = input("Enter extension: ") 
+    email= input("Enter email:  ") 
+    officeCode = input("Enter office code: ") 
+    reportsTo = input("Enter reportsTo: ") 
+    jobTitle = input("Enter job title: ")
+    query = "INSERT INTO employees VALUES (%s, %s, %s, %s,%s, %s, %s, %s)"
+    cursor = conn.cursor()
+    values = (employeeNumber, lastName, firstName, extension, email, officeCode, reportsTo, jobTitle)
+    cursor.execute(query, values)
+    conn.commit()
+    print(f"Employee '{firstName+lastName}' added successfully.")
+  
+def add_customer(conn):
+  customerNumber = input("Enter Customer ID: ")
+  customerName = input("Enter your name: ")
+  contactLastName = input("Enter contactLastName: ")
+  contactFirstName = input("Enter contactFirstName: ")
+  phone = input("Enter phoneNumber: ")
+  addressLine1 = input("EnteraddressLine: ")
+  addressLine2 = input("Enter addressLine2: ")
+  city = input("Enter city: ")
+  state = input("Enter state: ")
+  postalCode = input("Enter postalcode: ")
+  country = input("Enter country: ")
+  salesRepEmployeeNumber = input("Enter SalesRepEmployeeNumber: ")
+  creditLimit = input("Enter creditLimit: ")
   cursor = conn.cursor()
-  query = "INSERT INTO employees VALUES (%s, %s, %s, %s,%s, %s, %s, %s)"
-  values = (employeeNumber, lastName, firstName, extension, email, officeCode, reportsTo, jobTitle)
+  query = "INSERT INTO customers VALUES (%s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+  values = (customerNumber, customerName, contactLastName, contactFirstName, phone, addressLine1, addressLine2, city, state, postalCode, country, salesRepEmployeeNumber, creditLimit)
   cursor.execute(query, values)
   conn.commit()
-  print(f"Employee '{firstName,lastName}' added successfully.")
+  print(f"Customer '{customerName}' added successfully.")
+  
+def add_order(conn):
+  orderNumber  = input("Enter order number: ")
+  orderDate  = input("Enter order date: ")
+  requiredDate = input("Enter required date: ")
+  shippedDate  = input("Enter shipping date: ")
+  status = input("Enter status(shipped/cancelled)")
+  comments = input("Enter comments")
+  customerNumber = input("Enter customer number")
+  cursor = conn.cursor()
+  query = "INSERT INTO employees VALUES (%s, %s, %s, %s,%s, %s, %s)"
+  values = (orderNumber, orderDate, date(requiredDate), date(shippedDate), status, comments, customerNumber)
+  cursor.execute(query, values)
+  conn.commit()
+  print("Added successfully.")
+  
+def add_payment_details(conn, customernumber, checknumber, paymentdate, amount):
+  cursor = conn.cursor()
+  query = "INSERT INTO employees VALUES (%s, %s, %s, %s,%s, %s, %s, %s)"
+  values = (customernumber, checknumber, date(paymentdate), amount)
+  cursor.execute(query, values)
+  conn.commit()
+  print("Added successfully.")
+  
+  
+def show_stats(conn):
+  cursor = conn.cursor()
+  total_employees = cursor.execute("SELECT * FROM employees")
+  total_customers = cursor.execute("SELECT * FROM customers")
+  total_payment = cursor.execute("SELECT * FROM payments")
+  total_order = cursor.execute("SELECT * FROM orders")
+  total_product = cursor.execute("SELECT * FROM products;")
+  conn.commit()
+  print("Total Employees: ",total_employees)
+  print("Total Customer: ",total_customers)
+  print("Total payments: ",total_payment)
+  print("Total Order: ",total_order)
+  print("Total Products: ",total_product)
+  
+  
+def show_employees(conn):
+  cursor = conn.cursor()
+  cursor.execute("SELECT * FROM employees")
+  conn.commit()
   
